d62bef388565e75b4dbfd89426727440ed8c6f09
abhsys <abhiraami.2353002@srec.ac.in>
2025-12-30 11:17:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(name, price) {
+    const item = cart.find(p => p.name === name);
+
+    if (item) {
+        item.qty++;
+    } else {
+        cart.push({ name, price, qty: 1 });
+    }
+
+    updateCart();
+}
+
+function updateCart() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    let cartItems = document.getElementById("cart-items");
+    let total = 0;
+    let count = 0;
+
+    cartItems.innerHTML = "";
+
+    cart.forEach((item, index) => {
+        total += item.price * item.qty;
+        count += item.qty;
+
+        cartItems.innerHTML += `
+            <div class="cart-item">
+                <span>${item.name} (x${item.qty})</span>
+                <span>₹${item.price * item.qty}</span>
+                <button onclick="removeItem(${index})">❌</button>
+            </div>
+        `;
+    });
+
+    document.getElementById("total").innerText = total;
+    document.getElementById("count").innerText = count;
+}
+
+function removeItem(index) {
+    cart.splice(index, 1);
+    updateCart();
+}
+
+updateCart();
